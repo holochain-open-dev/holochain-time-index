873b4caea5ffc554b81a6988b7b1fe6646a42a86
jdeepee <joshuadparkin@gmail.com>
2021-03-02 16:29:30 +0000

    added very basic description of DNA

--- zomes/index/src/lib.rs
+++ zomes/index/src/lib.rs
@@ -6,6 +6,29 @@
 //!
 //! DNA functioning
 //!
+//! The main component that allows the mitigation of DHT hotspots is 1) the time delimited chunks and 2) the agent focused validation that occurs on each chunk. 
+//! For any given chunk an agent cannot make more than DIRECT_CHUNK_LINK_LIMIT direct links on a given chunk. Once this limit has been met, subsequent 
+//! links must be linked together in a linked list shape. Here the target entry of the last direct link they created is the source entry of the linked list. 
+//! An agent can make links like this until their total links reaches the ENFORCE_SPAM_LIMIT limit at which point no further links are allowed. 
+//! The first limit is a measure to protect DHT hotspots in a busy DHT with a high MAX_CHUNK_INTERVAL & the second limit is supposed to block clear/obvious spam.
+//! 
+//! This DNA's variables is expected to be static. That means its expected that the: DIRECT_CHUNK_LINK_LIMIT, ENFORCE_SPAM_LIMIT & MAX_CHUNK_INTERVAL will 
+//! stay the same throughout the lifetime of the DHT. This is done to make validation possible in situations where DHT could occur. 
+//! If limits are able to change; we have no way to reliably know if an agent is operating on old limits by consequence of being out of touch 
+//! with latest DHT state or if the agent is malicious and pretending they do not see the new limits.
+//! If you can guarantee that fragmentation of the DHT will not happen then its possible that limit updates could work. 
+//! We may also add support for the reduction of limits in the future; but increasing limits will not land.
+//! 
+//! For now if one wishes to increase these values its recommended to create a new DNA/DHT and link to it from the current. 
+//!
+//! This DNA exposes a few helper functions to make operating with chunked data easy. Ones of note are: 
+//! ['TimeChunk::get_current_chunk()'], ['TimeChunk::get_latest_chunk()'], ['TimeChunk::get_chunks_for_time_span()'], ['TimeChunk::add_link()'] & ['TimeChunk::get_links()']
+//! 
+//! ['TimeChunk::get_current_chunk()'] will take the current time as denoted by sys_time() and return null or a chunk that can be used to served entries for the current time.
+//! ['TimeChunk::get_latest_chunk()'] will search though the DNA's time "index" and find the last commited chunk and return it.
+//! ['TimeChunk::get_chunks_for_time_span()'] will return all chunks that served in a given time span
+//! ['TimeChunk::add_link()'] will create a link on a chunk. This will happen either directly or by the linked list fashion as explained above.
+//! ['TimeChunk::get_links()'] will get links from the chunk, recursing down any linked lists to ensure that all links are returned for a given chunk.
 
 #[macro_use]
 extern crate lazy_static;
